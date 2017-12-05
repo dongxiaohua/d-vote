1c77aabc17e65d40acc56b6979121479dee3b243
dongxiaohua <dongxiaohua@fxiaoke.com>
2017-12-05 12:34:11 +0800

    groovy

---
+++ src/test/groovy/com/dong/vote/mapperTest/MapperTest.groovy
@@ -0,0 +1,15 @@
+package com.dong.vote.mapperTest
+
+import lombok.extern.slf4j.Slf4j
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+/**
+ * Created by lirui on 2017-09-16 21:01.
+ */
+@ContextConfiguration(locations = "classpath:mapperContext.xml")
+@Slf4j
+class MapperTest extends Specification {
+
+
+}
