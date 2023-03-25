116e26a17cd426511fb3653e15bc38766fe61cd5
dongxiaohua <dong_healer@163.com>
2023-03-25 17:23:17 +0800

    lru

---
+++ src/main/java/com/dong/vote/study/service/LruService.java
@@ -0,0 +1,26 @@
+package com.dong.vote.study.service;
+
+/**
+ * LRU算法
+ *
+ * @author: dongxiaohua
+ * @date: 2023-03-25 17:20:50
+ */
+public class LruService {
+
+  public static class Entry {
+    public Entry pre;
+    public Entry next;
+    public int key;
+    public int value;
+
+    public Entry() {
+
+    }
+
+    public Entry(int key, int value) {
+      this.key = key;
+      this.value = value;
+    }
+  }
+}
